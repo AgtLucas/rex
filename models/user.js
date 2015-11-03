b4cfa45952bc1ff906c8e5f02e43b436356e12c8
Lucas <lucas@agtlucas.com>
2015-11-03 00:56:34 -0200

    Updates

--- models/user.js
+++ models/user.js
@@ -6,5 +6,9 @@ var schema = database.Schema;
 module.exports = database.model('User', schema({
   name: String,
   email: String,
+  skills: [{
+    name: String,
+    value: { type: Number, min: 0, max: 10 }
+  }],
   updated_at: { type: Date, default: Date.now }
 }));
