3ed4e21f689b70694055fff5e44cfa7eef08a46d
Lucas <lucas@agtlucas.com>
2015-11-02 23:30:23 -0200

    Updates

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
+
+// Routes
+var skills = require('./routes/skills');
+
+app.use('/api', router);
 
 app.get('/', function(req, res) {
   res.send('OK!!!');
 });
+
+app.use('/skills')
 
 var server = app.listen(3000, function() {
   var host = server.address().address;
